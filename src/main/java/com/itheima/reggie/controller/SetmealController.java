918f1e5d26bbaf0c9279cc3e6118038153f504dd
Xxxxic <2453915974@qq.com>
2024-03-02 22:08:53 +0800

    Add SelectList of Setmeal

--- src/main/java/com/itheima/reggie/controller/SetmealController.java
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Setmeal;
 import com.itheima.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,5 +53,23 @@ public class SetmealController {
         return R.success("删除成功");
     }
 
+    /**
+     * 根据分类信息获取套餐的数据
+     * GET setmeal/list?categoryId=1413342269393674242&status=1
+     * 接收参数：@RequestParam("categoryId") Long id, @RequestParam("status") int status
+     * 但是选择用Setmeal接收更好
+     */
+    @GetMapping("/list")
+    public R<List<Setmeal>> list(Setmeal setmeal) {
+        //log.info(id.toString());
+
+        LambdaQueryWrapper<Setmeal> qw = new LambdaQueryWrapper<>();
+        qw.eq(setmeal.getId() != null, Setmeal::getCategoryId, setmeal.getId())
+                .eq(setmeal.getStatus() != null, Setmeal::getStatus, setmeal.getStatus());
+        qw.orderByDesc(Setmeal::getUpdateTime);
+        List<Setmeal> list = setmealService.list(qw);
+
+        return R.success(list);
+    }
 
 }
